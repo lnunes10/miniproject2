43f398b17dfc67858c4d435f73dda8357fe377bf
Lorre <ln64@njit.edu>
2019-08-07 18:59:03 -0400

    my attempt at creating a test inserting a user into a users table

---
+++ tests/Unit/InsertUserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\User;
+use Illuminate\Support\Facades\App;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class InsertUserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function InsertUserTest()
+    {
+        $user= new user;
+        $this->assertAuthenticatedAs(true);
+    }
+}
